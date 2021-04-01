09930b96e4f8e4cbe3a371f2363b905acce6fda2
aaminamariam <22100062@lums.edu.pk>
2021-04-01 22:56:05 +0500

    authentication

--- StreetPizza_13th/settings.py
+++ StreetPizza_13th/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '71zmtg(*vhr()op!b0hcdlk@qvkbj^c)k=ddbhi+vnk8^hz$0$'
+from decouple import config
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
